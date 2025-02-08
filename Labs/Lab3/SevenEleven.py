ce6b36f912ff99d8f6581b4699c9df7da6e75522
Victor F <valfonsof1999@gmail.com>
2025-02-07 22:33:29 -0800

    Lab2 Upload

---
+++ Labs/Lab3/SevenEleven.py
@@ -0,0 +1,11 @@
+
+snacks = ['chips','cookies','pie']
+
+def snack_bar (choice):
+
+    if choice in snacks:
+        print(f"{choice} are in stock. Let me get that")
+    else:
+        print("sorry, we are out of that.")
+
+
